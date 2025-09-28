9526e591d79b60450575d7cb17bf40e17d864d7a
xiaoshi930i <huyanhongnet@163.com>
2025-09-28 21:54:16 +0800

    Update const.py

--- custom_components/qinhua_gas/const.py
+++ custom_components/qinhua_gas/const.py
@@ -8,9 +8,9 @@ CONF_CARD_ID = "card_id"
 CONF_XIUZHENG = "xiuzheng"
 CONF_TOKEN_S = "token_s"
 
-DEFAULT_USER_ID = "oh2fKv_y8EklO9SBi_sMtTgGhlns"
-DEFAULT_CARD_ID = "0842342294"
-DEFAULT_TOKEN_S = "4cb8b5dba52130ac0ce23d5eeea64dca"
+DEFAULT_USER_ID = "oh2fKv_y8EklO9SBi_sMtTgGhlns8"
+DEFAULT_CARD_ID = "08423422948"
+DEFAULT_TOKEN_S = "4cb8b5dba52130ac0ce23d5eeea64dca8"
 DEFAULT_XIUZHENG = 0
 
 API_ENDPOINT = "http://wkf.qhgas.com/rs/WX/searchInvoice"
